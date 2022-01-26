8d32cddd0d56a5e16551c7d4e9bc15a0a6c6ac3b
Tony Menza <ajm043@bucknell.edu>
2022-01-25 21:11:25 -0500

    completed 'useState obkect' lesson

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  // Alternative way no object
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    // Using the spread operator allows us to copy the contents of the person object into the new object and then immediately overwright the following value of our choice
+    // setPerson({ ...person, message: "hello world" });
+    // Alternative way no object
+    setMessage('hello world');
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
