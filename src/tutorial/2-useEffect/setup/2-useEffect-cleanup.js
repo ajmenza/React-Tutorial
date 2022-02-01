d4770e4fbeee2b61ca74a92faf5ef665ec925496
Tony Menza <ajm043@bucknell.edu>
2022-01-31 20:04:55 -0500

    completed useEffect lesson

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  // Need to remove our event listeners everytime we call otherwise we will keep creating event listeners everytime there is a rerender. In this case, we can use [] since we only need useEffect to run once (on render) to get the event listener, so we can comment out the clean up function. Still, the cleanup function is useful conditional rendering where we will need to remove the event listener
+  useEffect(() => {
+    console.log("use effect");
+    window.addEventListener("resize", checkSize);
+    // return () => {
+    //   console.log("cleanup");
+    //   window.removeEventListener("resize", checkSize);
+    // };
+  }, []);
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
